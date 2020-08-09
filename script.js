bce90d662687f31de38c6298a454492f8669b73d
wandaazhar007 <wandaazhar@gmail.com>
2020-08-09 22:50:58 +0700

    add simple vertical tab

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Mobile Navbar
 $(document).ready(function () {
   $('a[href="#navbar-more-show"], .navbar-more-overlay').on('click', function (event) {
     event.preventDefault();
@@ -8,5 +9,17 @@ $(document).ready(function () {
       $('a[href="#navbar-more-show"]').closest('li').removeClass('active');
     }
     return false;
+  });
+});
+
+// Simple Vertical Tab
+$(document).ready(function () {
+  $("div.bhoechie-tab-menu>div.list-group>a").click(function (e) {
+    e.preventDefault();
+    $(this).siblings('a.active').removeClass("active");
+    $(this).addClass("active");
+    var index = $(this).index();
+    $("div.bhoechie-tab>div.bhoechie-tab-content").removeClass("active");
+    $("div.bhoechie-tab>div.bhoechie-tab-content").eq(index).addClass("active");
   });
 });
